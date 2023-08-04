aadff98e580fb76cccfdcb7a604863081bc4813c
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-08-04 15:09:19 -0500

    resolve linting

--- sns.tf
+++ sns.tf
@@ -4,9 +4,9 @@ resource "aws_sns_topic" "this" {
 }
 
 resource "aws_sns_topic_subscription" "this" {
-  count                      = var.webhook_url != null ? 1 : 0
-  topic_arn                  = aws_sns_topic.this.arn
-  protocol                   = "https"
-  endpoint                   = var.webhook_url
-  endpoint_auto_confirms     = true
+  count                  = var.webhook_url != null ? 1 : 0
+  topic_arn              = aws_sns_topic.this.arn
+  protocol               = "https"
+  endpoint               = var.webhook_url
+  endpoint_auto_confirms = true
 }
