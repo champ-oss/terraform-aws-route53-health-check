5e2c653bdf56963b35edabd1b2c8ed14fa18d25a
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-03 10:32:44 -0500

    add intigration test

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -1,5 +1,18 @@
 set -e
 
-sleep 30
+for i in {1..12}; do
+  aws route53 get-health-check-status --health-check-id $ROUTE53_HEALTH_CHECK_ID | jq -e '.HealthCheckObservations[] | select(.StatusReport.Status)' | grep Success
+  result=$?
+  if [[ $result -eq 0 ]]
+  then
+    break
+  else
+    sleep 10
+  fi
+done
 
-aws route53 get-health-check-status --health-check-id $ROUTE53_HEALTH_CHECK_ID | jq -e '.HealthCheckObservations[] | select(.StatusReport.Status)' | grep Success
+if [[ $result -ne 0 ]]
+then
+  exit 1
+fi
+
