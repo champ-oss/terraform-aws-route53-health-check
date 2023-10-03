a144cc24912055da03be7376613831f00343ffff
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-03 10:12:28 -0500

    add intigration test

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -2,4 +2,4 @@ set -e
 
 sleep 30
 
-aws route53 get-health-check-status --health-check-id $R53_HEALTH_CHECK_ID | jq -e '.HealthCheckObservations[] | select(.StatusReport.Status)' | grep Success
+aws route53 get-health-check-status --health-check-id $ROUTE53_HEALTH_CHECK_ID | jq -e '.HealthCheckObservations[] | select(.StatusReport.Status)' | grep Success
