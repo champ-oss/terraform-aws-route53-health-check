ac835895a1b8444e03e19432d622c6f309c08fa5
plangley-champ <90263853+plangley-champ@users.noreply.github.com>
2023-10-03 11:57:07 -0500

    updating bash script

--- examples/complete/test.sh
+++ examples/complete/test.sh
@@ -1,5 +1,9 @@
 set -e -o pipefail
 
+
+
+sleep 600
+
 cmd=`aws route53 get-health-check-status --health-check-id $ROUTE53_HEALTH_CHECK_ID | jq -e '.HealthCheckObservations[] | select(.StatusReport.Status)' | grep Success`
 
 for i in 1 2 3 4 5 6 7 8 9 10; do command && break || sleep 60; done
